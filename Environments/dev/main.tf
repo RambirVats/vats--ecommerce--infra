c1803e83bdc7e7735be32841f76faf0b05d34e67
Vishnu <Vishnu@umeshchaudhary091991gmail.onmicrosoft.com>
2025-05-21 14:12:06 +0530

    ecommerceinfra

---
+++ Environments/dev/main.tf
@@ -0,0 +1,21 @@
+variable "rg_name" {}
+variable "rg_location" {}
+
+
+module "resource_group" {
+    source = "../../module/resource_group"
+    resource_group_name = var.rg_name
+    resource_group_location = var.rg_location
+}
+
+module "storage_account" {
+    depends_on = [ module.resource_group ]
+    source = "../../Module/Storage_account"
+    storage_account_name = "stecommdev"
+    resource_group_name = "rg-dev-ecomm-01"
+    location = "East US"
+}
+
+variable "storage_account_name" {}
+variable "resource_group_name" {}
+variable "location" {}
---
+++ Environments/dev/terraform.tfvars
@@ -0,0 +1,2 @@
+rg_name = "rg-dev-ecomm-01"
+rg_location = "East US"
